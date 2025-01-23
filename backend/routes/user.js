810c0a0d206132815db9dd6adbc94f6bece5ad56
swastikCommits <textswastik.alt@gmail.com>
2025-01-23 18:50:08 +0530

    added user signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,50 @@
 const express=require('express');
 const router = express.Router();
 const zod=require('zod');
+const { User } = require('../db');
+const { JWT_SECRET } = require('../config');
+const jwt=require("jsonwebtoken");
+
+const signUpBody=zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+    lastName: zod.string(),
+    firstName: zod.string()
+
+})
+router.post('/signup',async function(req, res){
+    const response=signUpBody.safeParse(req.body);
+    if(!response.success){
+        return res.status(411).json({
+            message: "Email already taken / Incorrent inputs"
+        })
+    }
+    
+    
+    const existing= await UserActivation.findOne({usernme: req.body.username});
+    if(existing){
+        res.status(411).json({
+            message: "Email already taken / Incorrent inputs"
+        })
+    }
+
+    
+    
+    const user= await User.create({
+        username: req. body.username,
+        password: req.body.password,
+        lastName: req.body.lastName,
+        firstName: req.body.firstName
+    })
+    const userId= user._id;
+    const token=jwt.sign({
+        userId
+    }, JWT_SECRET);
+    res.json({
+        message: "User created successfully",
+        token
+    })
+})
 
 
 module.exports = router;
